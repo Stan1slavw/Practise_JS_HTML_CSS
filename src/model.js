f8916d461377ee16ac7baa97ade007e32325c882
Stan1slav <stas1234713@gmail.com>
2023-08-20 21:35:07 +0300

    dev-- add new js file 'blocks.js' and refactor code with adding classes

--- src/model.js
+++ src/model.js
@@ -1,20 +1,47 @@
 import image from './assets/image.png'
-
+import {Block} from './classes/blocks'
 export const model = [
-    {
-        type: 'title', value: 'Конструктор сайтов на чистом JavaScript', options: {
-            tag: 'h2',
-            styles: `background: linear-gradient(to right, #ff0099, #493240); color: fff;`
+    new Block('title', 'Конструктор сайтов на чистом JavaScript', {
+        tag: 'h2',
+        styles: {
+            background: 'linear-gradient(to right, #ff0099, #493240);',
+            color: '#fff',
+            padding: '1.5rem',
+            'text-align': 'center'
         }
-    },
-    {type: 'text', value: 'here we go with some text'},
-    {
-        type: 'columns', value: [
-            '1111111111',
-            '2222222222',
-            '3333333333',
-            '5555555555'
-        ]
-    },
-    {type: 'image', value: image}
+    }),
+
+    new Block('image', image, {
+        styles: {
+            padding: '2rem 0',
+            display: 'flex',
+            'justify-content': 'center'
+        },
+        imageStyles: {
+            width: '500px',
+            height: 'auto'
+        },
+        alt: 'Это картинка'
+    }),
+
+    new Block('columns',[
+        'Приложение на чистом JavaScript, без использования библиотек',
+        'Узнаешь как работают принципы SOLID и ООП в JavaScript за один курс'
+    ], {
+        styles: {
+            background: 'linear-gradient(to bottom, #8e2de2, #4a00e0);',
+            padding: '2rem',
+            color: '#fff',
+            'font-weight': 'bold'
+        }
+    }),
+
+    new Block('text', 'Здарова славяне', {
+        styles: {
+            background: 'linear-gradient(to left, #f2994a, #f2c94e);',
+            padding: '1rem',
+            'font-weight': 'bold'
+        }
+    }),
+
 ]
--- src/templates.js
+++ src/templates.js
@@ -1,23 +1,23 @@
-import {row,col} from "./utils";
+import {row, col, css} from "./utils";
 
 function title(block) {
-    const tag = block.options.tag ?? 'h1';
-    const styles = block.options.styles;
-    return row(col(`<${tag}>${block.value}</${tag}>`), styles);
+    const {tag = 'h1', styles} = block.options
+    return row(col(`<${tag}>${block.value}</${tag}>`), css(styles));
 }
 
- function text(block){
-    return row(col(`<p>${block.value}</p>`));
+function text(block) {
+    return row(col(`<p>${block.value}</p>`), css(block.options.styles));
 }
 
- function columns(block){
+function columns(block) {
     const html = block.value.map(item => col(item))
 
-    return row(`${html.join('')}`);
+    return row(`${html.join('')}`, css(block.options.styles));
 }
 
- function image(block){
-    return row(`<img src=${block.value}>`);
+function image(block) {
+    const {imageStyles, alt, styles} = block.options
+    return row(`<img src="${block.value}" alt="${alt}" style="${css(imageStyles)}">`, css(styles));
 }
 
 export const templates = {
